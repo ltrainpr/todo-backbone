d40680a94f3761774d65c307f4b74353bac85e4b
Lionel E. Ramos <ltrainpr@gmail.com>
2013-12-11 09:54:53 -0700

    generates app skeleton (main controller and index view)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 TodoBackbone::Application.routes.draw do
+  root to: "main#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
