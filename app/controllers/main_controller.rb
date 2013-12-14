374e415007e01daaa4466a079de8dd7a933e6c03
Lionel E. Ramos <ltrainpr@gmail.com>
2013-12-14 00:07:20 -0700

    adds controllers

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,33 @@
 class MainController < ApplicationController
+  
   def index
+  	Todo.all
+  end
+
+  def show
+  	find
+  end
+
+  def create
+  	Todo.create(todo_params)
+  end
+
+  def update
+  	find
+  	@todo.update_attributes!(todo_params)
+  end
+
+  def destroy
+  	Todo.destroy(params[:id])
+  end
+
+  private
+
+  def todo_params
+  	params.require(:todo).permit(:task, :complete)
+  end
+
+  def find
+  	@todo = Todo.find(params[:id])
   end
 end
