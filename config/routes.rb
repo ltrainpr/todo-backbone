1de96a423335960b97daeb90b9f1044815a57464
Lionel E. Ramos <ltrainpr@gmail.com>
2013-12-14 00:10:11 -0700

    adds todo routes and seeds db

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 TodoBackbone::Application.routes.draw do
+  resources :todos
+
   root to: "main#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Todo.create!(task: "Laundry", complete: false)
+Todo.create!(task: "Exercise", complete: false)
+Todo.create!(task: "Volunteer", complete: false)
+Todo.create!(task: "Groceries")
+Todo.create!(task: "Call Grandma")
+Todo.create!(task: "Do something nice for wife")
+Todo.create!(task: "Read Design Patterns in Ruby")
+Todo.create!(task: "Build a Backbone.js app")
