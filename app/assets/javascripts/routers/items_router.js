ecda438f9baea498dab0258e1329b031368eacfd
Lionel E. Ramos <ltrainpr@gmail.com>
2013-12-16 14:19:28 -0700

    Adds initialize function to Router and View. Changes render function for collection

--- app/assets/javascripts/routers/items_router.js
+++ app/assets/javascripts/routers/items_router.js
@@ -1,8 +1,17 @@
 TodoBackbone.Routers.Items = Backbone.Router.extend({
-	routes: {'': 'index', "items/:id": "show"},
+	routes: {'': 'index', "todos/:id": "show"},
 	
+	initialize: function () {
+		this.collection = new TodoBackbone.Collections.Items();
+		return this.collection.fetch({
+			reset: true
+		});
+	},
+
 	index: function() {
-		var view = new TodoBackbone.Views.ItemsIndex();
+		var view = new TodoBackbone.Views.ItemsIndex({
+			collection: this.collection
+		});
 		return $('#container').html(view.render().el);
 	},
 	show: function(id) {
--- app/assets/javascripts/views/items/items_index.js
+++ app/assets/javascripts/views/items/items_index.js
@@ -2,8 +2,12 @@ TodoBackbone.Views.ItemsIndex = Backbone.View.extend({
 
   template: JST['items/index'],
 
+  initialize: function () {
+		return this.collection.on('reset', this.render, this);
+  },
+
   render: function() {
-		$(this.el).html(this.template({items: "Items go here"}));
+		$(this.el).html(this.template({todos: this.collection}));
 		return this;
   }
 });
