40f74e70b0eb24e7e07a11a0b9d01393044ff3ef
Lionel E. Ramos <ltrainpr@gmail.com>
2013-12-16 14:25:30 -0700

    Todo controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,33 @@
 class TodosController < ApplicationController
+	
+	def index
+		@todos = Todo.all
+		render json: @todos
+	end
+
+	def show
+		@todo = Todo.find(params[:id])
+		render json: @todo
+	end
+
+	def create
+		@todo = Todo.create(todos_params)
+		render json: @todo
+	end
+
+  def update
+    @todo = Todo.find(params[:id])
+    render @todo.update_attributes!(todos_params)
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy(todos_params)
+  end
+
+	private
+
+	def todos_params
+		params.require(:todo).permit(:task, :complete)
+	end
 end
